b5f3ad5123611833526174379a2c5cf2eab34ccc
Gianfrano Giordano <gianfranogiordano@gmail.com>
2024-03-09 14:40:11 -0500

    cambio de api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://api.soyclickstore.com/api/v1',
+  base_url: 'https://elb.soyclickstore.com/api/v1',
   wsUrl: 'https://api.soyclickstore.com',
   cloudFront: 'https://d3cj1y2eynyat2.cloudfront.net',
   clickstoreId: '65cbb4aef603ea05486973f6',
